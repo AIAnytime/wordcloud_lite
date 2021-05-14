133a9a29d059cf2155bdd617b8c64b76eea2c100
Sonu Kumar <sonu1000raw@gmail.com>
2021-05-14 18:19:56 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'wordcloud_lite',         
+  name = 'wordcloud_lite',    
+  long_description=long_description,
+  long_description_content_type='text/markdown',    
   packages = ['wordcloud_lite'],   
-  version = '1.1',      
+  version = '1.2',      
   license='MIT',        
   description = 'Generate Word Cloud with ease.',   
   author = 'Sonu Kumar',                   
